1d6483019a0af2656d0db8cb3be57fb9872fdc9f
Peter Willemsen <peter@codebuffet.co>
2026-07-02 06:29:10 +0000

    Added skill

--- ggml/src/ggml-metal/ggml-metal-ops.cpp
+++ ggml/src/ggml-metal/ggml-metal-ops.cpp
@@ -613,7 +613,6 @@ int ggml_metal_op_repeat(ggml_metal_op_t ctx, int idx) {
         /*.nb1  =*/ nb1,
         /*.nb2  =*/ nb2,
         /*.nb3  =*/ nb3,
-        /*.keep_intermediates =*/ 0,
     };
 
     ggml_metal_encoder_set_pipeline(enc, pipeline);
@@ -1648,6 +1647,7 @@ int ggml_metal_op_gated_delta_net(ggml_metal_op_t ctx, int idx) {
         /*.nb1  =*/ nb1,
         /*.nb2  =*/ nb2,
         /*.nb3  =*/ nb3,
+        /*.keep_intermediates =*/ 0,
     };
 
     ggml_metal_encoder_set_pipeline(enc, pipeline);
